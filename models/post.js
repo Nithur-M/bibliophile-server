aaab27b746bf30a94df75c4fc841063c3733fd69
unknown <nithur97@gmail.com>
2023-02-02 22:30:18 +0530

    changed model name to post message

--- models/post.js
+++ models/post.js
@@ -17,6 +17,6 @@ const postSchema = mongoose.Schema({
 
 });
 
-const Post = mongoose.model('Post', postSchema);
+const PostMessage = mongoose.model('Post', postSchema);
 
-export default Post;
+export default PostMessage;
