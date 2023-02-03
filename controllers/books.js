81f18da492568c43dc7ee0462a892a9b41b6e2e9
kiruba094 <kirupakaran094@gmail.com>
2023-02-03 18:14:24 +0530

    fix book search query

--- controllers/books.js
+++ controllers/books.js
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export const search = async (req, res) => {
-    console.log(req)
-    try {
-        const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${req.query}&maxResults=6`);
-        const results = await response.json();
-        res.status(201).json(results.items);
-    } catch (error) {
-        res.status(409).json({ message: error.message });
-    }
-}
+  console.log(req.query.query);
+  try {
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${req.query.query}&maxResults=6`);
+    const results = await response.json();
+    res.status(201).json(results.items);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
