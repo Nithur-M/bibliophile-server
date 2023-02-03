4b460123f9c3d62502a9d845b6fcdc27aa1255e8
unknown <nithur97@gmail.com>
2023-02-03 10:48:05 +0530

    export app in index.js

--- index.js
+++ index.js
@@ -30,3 +30,6 @@ const PORT = process.env.PORT || 3001;
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error) => console.log(error.message));
+
+// Export the Express API
+export default app;
