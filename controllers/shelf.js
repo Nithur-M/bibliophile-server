339603018b4b42748d8c95e9a5926435cc39fd99
unknown <nithur97@gmail.com>
2023-03-06 22:03:38 +0530

    pop book when changing category

--- controllers/shelf.js
+++ controllers/shelf.js
@@ -12,21 +12,41 @@ export const addBook = async (req, res) => {
             id: req.body.id,
             addedDate: new Date()
         };
+
+        // Check if the book is already in any category and remove it from that category
+        for (let i = 0; i < shelf.readBooks.length; i++) {
+            if (shelf.readBooks[i].id === book.id) {
+                shelf.readBooks.splice(i, 1);
+                break;
+            }
+        }
+        for (let i = 0; i < shelf.currentlyReadingBooks.length; i++) {
+            if (shelf.currentlyReadingBooks[i].id === book.id) {
+                shelf.currentlyReadingBooks.splice(i, 1);
+                break;
+            }
+        }
+        for (let i = 0; i < shelf.toBeReadBooks.length; i++) {
+            if (shelf.toBeReadBooks[i].id === book.id) {
+                shelf.toBeReadBooks.splice(i, 1);
+                break;
+            }
+        }
         switch (req.params.category) {
-        case 'read':
-            shelf.readBooks.push(book);
-            break;
-        case 'currentlyReading':
-            shelf.currentlyReadingBooks.push(book);
-            break;
-        case 'toBeRead':
-            shelf.toBeReadBooks.push(book);
-            break;
-        default:
-            return res.status(400).send({ error: 'Invalid category' });
+            case 'read':
+                shelf.readBooks.push(book);
+                break;
+            case 'currentlyReading':
+                shelf.currentlyReadingBooks.push(book);
+                break;
+            case 'toBeRead':
+                shelf.toBeReadBooks.push(book);
+                break;
+            default:
+                return res.status(400).send({ error: 'Invalid category' });
         }
         await shelf.save();
-        res.status(201).json(shelf);
+        res.status(201).json({message: "successfully added to your shelf"});
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
