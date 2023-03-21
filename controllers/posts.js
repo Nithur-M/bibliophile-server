fde28694bf94b0f2bba42eea9e56ef6e69dd6a57
unknown <nithur97@gmail.com>
2023-03-21 23:14:24 +0530

    fixed save

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,103 +1,93 @@
-import express from "express";
-import mongoose from "mongoose";
+import express from 'express';
+import mongoose from 'mongoose';
 
-import PostMessage from "../models/post.js";
+import PostMessage from '../models/post.js';
 
 const router = express.Router();
 
 export const getPosts = async (req, res) => {
-  const { page } = req.query;
+    const { page } = req.query;
+    
+    try {
+        const LIMIT = 8;
+        const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
+    
+        const total = await PostMessage.countDocuments({});
+        const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
 
-  try {
-    const LIMIT = 8;
-    const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
-
-    const total = await PostMessage.countDocuments({});
-    const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
-
-    res.json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-};
+        res.json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT)});
+    } catch (error) {    
+        res.status(404).json({ message: error.message });
+    }
+}
 
 export const getPostsBySearch = async (req, res) => {
-  const { searchQuery, tags } = req.query;
+    const { searchQuery, tags } = req.query;
 
-  try {
-    const title = new RegExp(searchQuery, "i");
+    try {
+        const title = new RegExp(searchQuery, "i");
 
-    const posts = await PostMessage.find({ $or: [{ title }, { tags: { $in: tags.split(",") } }] });
+        const posts = await PostMessage.find({ $or: [ { title }, { tags: { $in: tags.split(',') } } ]});
 
-    res.json({ data: posts });
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-};
+        res.json({ data: posts });
+    } catch (error) {    
+        res.status(404).json({ message: error.message });
+    }
+}
 
 export const getPostsByCreator = async (req, res) => {
-  const { creator } = req.query;
+    const { name } = req.query;
 
-  try {
-    const posts = await PostMessage.find({ creator });
+    try {
+        const posts = await PostMessage.find({ name });
 
-    res.json({ data: posts });
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-};
+        res.json({ data: posts });
+    } catch (error) {    
+        res.status(404).json({ message: error.message });
+    }
+}
 
-export const getPost = async (req, res) => {
-  const { id } = req.params;
+export const getPost = async (req, res) => { 
+    const { id } = req.params;
 
-  try {
-    const post = await PostMessage.findById(id);
-
-    res.status(200).json(post);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-};
+    try {
+        const post = await PostMessage.findById(id);
+        
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
 
 export const createPost = async (req, res) => {
-  const auth = req.currentUser;
-  const post = req.body;
+    const auth = req.currentUser;
+    const post = req.body;
 
-  const newPostMessage = new PostMessage({ ...post, creator: auth.uid, createdAt: new Date().toISOString() });
+    const newPostMessage = new PostMessage({ ...post, creator: auth.uid, createdAt: new Date().toISOString() })
 
-  try {
-    await newPostMessage.save();
+    try {
+        await newPostMessage.save();
 
-    res.status(201).json({ message: "Your post was successful. Thank you for sharing!" });
-  } catch (error) {
-    res.status(409).json({ message: error.message });
-  }
-};
+        res.status(201).json(newPostMessage);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
 
 export const updatePost = async (req, res) => {
-  const { id } = req.params;
-  const { title, message, creator, selectedFile, tags } = req.body;
+    const { id } = req.params;
+    const { title, message, creator, selectedFile, tags } = req.body;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
 
-  const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
 
-  await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
-
-  res.json(updatedPost);
-};
+    res.json(updatedPost);
+}
 
 export const deletePost = async (req, res) => {
-  const { id } = req.params;
-
-  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
-
-  await PostMessage.findByIdAndRemove(id);
-
-  res.json({ message: "Post deleted successfully." });
-};
-
-export const savePost = async (req, res) => {
     const { id } = req.params;
 
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
@@ -107,41 +97,73 @@ export const savePost = async (req, res) => {
     res.json({ message: "Post deleted successfully." });
 }
 
-export const likePost = async (req, res) => {
+export const savePost = async (req, res) => {
+    const auth = req.currentUser;
     const { id } = req.params;
 
-    if (!req.userId) {
+    if (!auth) {
         return res.json({ message: "Unauthenticated" });
       }
 
-  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-  const post = await PostMessage.findById(id);
+    const post = await PostMessage.findById(id);
 
-    const index = post.likes.findIndex((id) => id ===String(req.userId));
+    const index = post.saves.findIndex((id) => id ===String(auth.uid));
 
     if (index === -1) {
-      post.likes.push(req.userId);
+      post.saves.push(auth.uid);
     } else {
-      post.likes = post.likes.filter((id) => id !== String(req.userId));
+      post.saves = post.saves.filter((id) => id !== String(auth.uid));
     }
 
-  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
 
-  res.status(200).json(updatedPost);
-};
+    res.status(200).json(updatedPost);
+}
+
+export const likePost = async (req, res) => {
+    const auth = req.currentUser;
+    const { id } = req.params;
+
+    if (!auth) {
+        return res.json({ message: "Unauthenticated" });
+      }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    
+    const post = await PostMessage.findById(id);
+
+    const index = post.likes.findIndex((id) => id ===String(auth.uid));
+
+    if (index === -1) {
+      post.likes.push(auth.uid);
+    } else {
+      post.likes = post.likes.filter((id) => id !== String(auth.uid));
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+
+    res.status(200).json(updatedPost);
+}
 
 export const commentPost = async (req, res) => {
+    const auth = req.currentUser;
     const { id } = req.params;
     const { value } = req.body;
 
-  const post = await PostMessage.findById(id);
+    if (!auth) {
+        return res.json({ message: "Unauthenticated" });
+      }
 
-  post.comments.push(value);
 
-  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+    const post = await PostMessage.findById(id);
 
-  res.json(updatedPost);
+    post.comments.push(value);
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+
+    res.json(updatedPost);
 };
 
 export default router;
